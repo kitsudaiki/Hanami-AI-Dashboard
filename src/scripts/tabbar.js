851dad6d03bc53cef317f1bcde32899337090fbc
Tobias Anker <tobias.anker@kitsunemimi.moe>
2021-07-04 17:58:40 +0200

    update tab-view

--- src/scripts/tabbar.js
+++ src/scripts/tabbar.js
@@ -1,21 +1,56 @@
-function openCity(evt, cityName) {
-  // Declare all variables
-  var i, tabcontent, tablinks;
+// Apache License Version 2.0
 
-  // Get all elements with class="tabcontent" and hide them
-  tabcontent = document.getElementsByClassName("tabcontent");
-  for (i = 0; i < tabcontent.length; i++) {
-    tabcontent[i].style.display = "none";
-  }
+// Copyright 2020 Tobias Anker
 
-  // Get all elements with class="tablinks" and remove the class "active"
-  tablinks = document.getElementsByClassName("tablinks");
-  for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].className = tablinks[i].className.replace(" active", "");
-  }
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
 
-  // Show the current tab, and add an "active" class to the button that opened the tab
-  document.getElementById(cityName).style.display = "block";
-  evt.currentTarget.className += " active";
+//     http://www.apache.org/licenses/LICENSE-2.0
+
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+var defaultTabName = "global_settings";
+var defaultTabIndex = 0;
+
+function resetAllTabs() 
+{
+    // Declare all variables
+    var i, tabcontent, tablinks;
+
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+}
+
+function selectTable(evt, tableName) 
+{
+    resetAllTabs();
+
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById(tableName).style.display = "block";
+    evt.currentTarget.className += " active";
 } 
- 
+
+function activateDefaultTab() 
+{
+    resetAllTabs();
+
+    document.getElementById(defaultTabName).style.display = "block";
+    tablinks = document.getElementsByClassName("tablinks");
+    tablinks[defaultTabIndex].className += " active";
+}
+
+activateDefaultTab();
